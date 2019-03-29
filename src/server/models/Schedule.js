b573a2f45749298fd9f636307440e3cce604939f
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-03-29 00:48:47 -0300

    Added: _procedure_id, status and datetime fields in Schedule model

--- src/server/models/Schedule.js
+++ src/server/models/Schedule.js
@@ -9,6 +9,23 @@ const ScheduleSchema = new Schema({
     type: Schema.Types.ObjectId,
     required: [true, 'É necessário informar quem irá atender'],
   },
+  _procedure_id: {
+    type: Schema.Types.ObjectId,
+    required: [true, 'É necessário informar o procedimento a ser realizado'],
+  },
+  status: {
+    type: String,
+    enum: [
+      'NEW',
+      'CONFIRMED',
+      'CANCELLED',
+    ],
+    default: 'NEW'
+  },
+  datetime: {
+    type: Date,
+    required: [true, 'É necessário informar o dia e o horário a serem marcados'],
+  }
 });
 
 export default model('Schedule', ScheduleSchema);
