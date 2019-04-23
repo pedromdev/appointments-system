9bcff950166ed6ccfd3ca8f289957d9333e8fd68
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-04-23 00:56:23 -0300

    Added: ApolloProvider

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { render } from 'react-dom';
+import ApolloClient from 'apollo-client';
+import {ApolloProvider} from 'react-apollo';
 
 import {LazyImport} from './src/helpers';
 import AppProvider from './src/components/AppProvider/AppProvider';
@@ -8,23 +10,36 @@ import { NotFound, BackendError, Signup, Signin, PasswordReset } from './src/pag
 
 import registerServiceWorker from './registerServiceWorker';
 
+import './src/assets/scss/main.scss';
+
 const Dashboard = LazyImport({
   loader: () => import('./src/containers/Dashboard')
 });
 
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
+
+let Root = () => (
+  <ApolloProvider client={client}>
+    <AppProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/404" component={NotFound} />
+          <Route exact path="/500" component={BackendError} />
+          <Route exact path="/forgot" component={PasswordReset} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
+          <Route path="/" component={Dashboard} />
+        </Switch>
+      </Router>
+    </AppProvider>
+  </ApolloProvider>
+);
+
 render(
-  <AppProvider>
-    <Router>
-      <Switch>
-        <Route exact path="/404" component={NotFound} />
-        <Route exact path="/500" component={BackendError} />
-        <Route exact path="/forgot" component={PasswordReset} />
-        <Route exact path="/signin" component={Signin} />
-        <Route exact path="/signup" component={Signup} />
-        <Route path="/" component={Dashboard} />
-      </Switch>
-    </Router>
-  </AppProvider>
-  , document.getElementById('root'));
+  <Root/>,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
