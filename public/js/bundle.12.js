4c64d2d9b679d73000189566107997ea6db571ba
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-05-02 01:42:40 -0300

    Updated: public files

---
+++ public/js/bundle.12.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{795:function(n){n.exports={form:{name:{label:"Name",input:"Name"}}}}}]);
+//# sourceMappingURL=bundle.12.js.map?version=0b10440fa57aa6df3a4a
---
+++ public/js/bundle.13.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{796:function(o){o.exports={form:{name:{label:"Nome",input:"Nome"}}}}}]);
+//# sourceMappingURL=bundle.13.js.map?version=0466a684aebab07819ac
