1c7473bda1d64c17acd10eb9485133cb48e4502d
Pedro Marcelo <ciseducarnet01@febracis.com.br>
2020-03-08 22:01:10 -0300

    Updated: map request to GraphQL context

--- src/server/apollo.js
+++ src/server/apollo.js
@@ -1,13 +1,12 @@
 import { ApolloServer } from 'apollo-server-express'
 import Schema from './graphql/Schema'
+import pick from 'lodash.pick'
 
 const apollo = new ApolloServer({
   schema: Schema,
   playground: process.env.NODE_ENV === 'development',
   context({ req }) {
-    return {
-      user: req.user
-    }
+    return pick(req, ['user', 'token'])
   }
 })
 
